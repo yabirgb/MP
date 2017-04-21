68f099e6e7c8d0c766df8b2dfaac42746e408511
JavierGalvez <javigalvezobispo@gmail.com>
2017-04-21 22:46:18 +0200

    arte ascii finiquitado

--- practica4/normal/src/testarteASCII.cpp
+++ practica4/normal/src/testarteASCII.cpp
@@ -12,74 +12,78 @@
 using namespace std;
 
 int main(){
-	const int MAX=4500;
-    char arteASCII[MAX+1]; // 4500 + el \0
-    Imagen origen;
+  const int MAX=4500;
+  char arteASCII[MAX+1]; // 4500 + el \0
+  Imagen origen;
 
-		//Lectura
-		const int MAX_NOMBRE = 180;
-		char fichero[MAX_NOMBRE], gris[MAX_NOMBRE], salida[MAX_NOMBRE];
-		int lineas_grises;
+  //Lectura
+  const int MAX_NOMBRE = 180;
+  char fichero[MAX_NOMBRE], gris[MAX_NOMBRE], salida[MAX_NOMBRE];
+  int lineas_grises;
 
-		cout << "Introduzca nombre de la imagen: ";
-		cin.getline(fichero, MAX_NOMBRE);
-		cout << "Introduzca el fichero de grises: ";
-		cin.getline(gris, MAX_NOMBRE);
-		cout << "Introduzca el fichero de salida: ";
-		cin.getline(salida, MAX_NOMBRE);
+  cout << "Introduzca nombre de la imagen: ";
+  cin.getline(fichero, MAX_NOMBRE);
+  if (!origen.leerImagen(fichero)){
+    cerr << "error leyendo "<< fichero << endl;
+    return 1;
+  }
+  else{
 
-		cout << fichero << " " << gris << " " << salida;
+    cout << "Imagen " << fichero << " leida correctamente" << endl;
+    cout << "Introduzca el fichero de grises: ";
+    cin.getline(gris, MAX_NOMBRE);
+    cout << "Introduzca el fichero de salida: ";
+    cin.getline(salida, MAX_NOMBRE);
 
-		ifstream fentrada;
-		fentrada.open(gris);
-		if (fentrada){
-			fentrada >> lineas_grises;
-			for (int i = 0; i < lineas_grises; i++){
-				fentrada >> gris;
-
-				if (!origen.leerImagen(fichero)){
-			   cerr << "error leyendo "<< fichero << endl;
-			   return 1;
-		    }
-				//Falta manehar errores de la escritura.
-				if(origen.aArteASCII(gris, arteASCII, MAX)){
-						cout << "arteASCII con la escala de grises número " << i << endl;
-						ofstream fsalida;
-						fsalida.open(salida, ios::app);
-						fsalida << "La imagen en arte ASCII (" << i << ") es:" << endl;
-						fsalida << arteASCII << endl;
-						fsalida.close();
-				}
-				else
-						cerr << "La conversión no ha sido posible" << endl;
-
-			}
-
-			fentrada.close();
-		}
-		else{
-			cerr << "Error de lectura del fichero. \n";
-		}
-
-
-		/*
-
-    // Leer la imagen gio.pgm
-    if (!origen.leerImagen(imagen1)){
-	   cerr << "error leyendo "<< imagen1 << endl;
-	   return 1;
+    ifstream fentrada;
+    fentrada.open(gris);
+    if (fentrada){
+      fentrada >> lineas_grises;
+      for (int i = 0; i < lineas_grises; i++){
+	fentrada >> gris;
+      
+	if(origen.aArteASCII(gris, arteASCII, MAX)){
+	  ofstream fsalida;
+	  fsalida.open(salida, ios::app);
+	  if(fsalida){
+	    fsalida << "La imagen en arte ASCII (" << i << ") es:" << endl;
+	    fsalida << arteASCII << endl;
+	  }
+	  else{
+	    cerr << "Fallo con el fichero de salida" << endl;
+	  }
+	  fsalida.close();
+	}
+	else{
+	  cerr << "La conversión no ha sido posible" << endl;
+	}
+      }
+      fentrada.close();
     }
+    else{
+      cerr << "Error de apertura del fichero. \n";
+    }
+  }
 
-    cout << "\nLa imagen en arte ASCII es:\n";
-    if(origen.aArteASCII(grises, arteASCII, MAX))
-        cout << arteASCII;
-    else
-        cout << "La conversión no ha sido posible" << endl;
 
-    cout << "Ahora Forzamos que no quepa. Debe aparecer un mensaje de error\n";
-    if(origen.aArteASCII(grises, arteASCII, MAX/10))
-        cout << arteASCII;
-    else
-        cout << "La conversión no ha sido posible" << endl;
-		*/
+  /*
+
+  // Leer la imagen gio.pgm
+  if (!origen.leerImagen(imagen1)){
+  cerr << "error leyendo "<< imagen1 << endl;
+  return 1;
+  }
+
+  cout << "\nLa imagen en arte ASCII es:\n";
+  if(origen.aArteASCII(grises, arteASCII, MAX))
+  cout << arteASCII;
+  else
+  cout << "La conversión no ha sido posible" << endl;
+
+  cout << "Ahora Forzamos que no quepa. Debe aparecer un mensaje de error\n";
+  if(origen.aArteASCII(grises, arteASCII, MAX/10))
+  cout << arteASCII;
+  else
+  cout << "La conversión no ha sido posible" << endl;
+  */
 }
