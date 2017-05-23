23e1710cc045aebefbb7bfa6c861ca47c6301e2f
Yábir <yabirgb@gmail.com>
2017-05-23 22:56:43 +0200

    Some errors left but work is done

--- practica6/src/testMorphing.cpp
+++ practica6/src/testMorphing.cpp
@@ -26,46 +26,67 @@ int main(int nargs, char** args){
 	const int MAX_NOMBRE = 150;
 	char fichero1[MAX_NOMBRE], fichero2[MAX_NOMBRE], destino[MAX_NOMBRE];
 	int npasos;
+	Lista * lista = new Lista;
 	
 	if(nargs != 5){
 		cerr << "Error en el número de argumentos" << endl;
 		return 1;
 	}
 
-	Imagen * imagen1, imagen2 = new Imagen;
-	
+	Imagen * imagen1 = new Imagen;
+	Imagen * imagen2 = new Imagen;
 	strcpy(fichero1,args[1]);
 	strcpy(fichero2,args[2]);
 	
-	if (!imagen1.leerImagen(fichero1)){
+	if (!imagen1->leerImagen(fichero1)){
     	cerr << "error leyendo "<< fichero1 << endl;
+		delete imagen1;
+		delete imagen2;
     	return 1;
   	}
-  	if (!imagen2.leerImagen(fichero2)){
+  	if (!imagen2->leerImagen(fichero2)){
     	cerr << "error leyendo "<< fichero2 << endl;
+		delete imagen1;
+		delete imagen2;
     	return 1;
   	}
 	
-	Imagen * img_salida = new Imagen;
 	npasos = charToInt(args[3]);
+	cout << npasos << endl;
 	// Hay que comprobar que sean del mismo tamaño
 	for(int k = 0; k < npasos; k++){
-		for(int i = 0; i < imagen1.filas(); i++){
-			for(int j = 0; j < imagen1.columnas(); j++){
-				Byte pixel = imagen1.get(j,i) + (k * (imagen2.get(j,i) - imagen1.get(j,i)) / npasos;
-				img_salida.set(j,i,pixel);
+		Imagen * img_salida = new Imagen(imagen1->filas(), imagen1->columnas());
+		for(int i = 0; i < imagen1->filas(); i++){
+			for(int j = 0; j < imagen1->columnas(); j++){
+				Byte pixel = imagen1->get(j,i) + (k * (imagen2->get(j,i) - imagen1->get(j,i)) / npasos);
+				img_salida->set(j,i,pixel);
 			}
 		}
+		lista->insertarFinal(*img_salida);
+		delete img_salida;
 	}
 	
 	strcpy(destino,args[4]);
 	
-	if (destino.escribirImagen(img_salida, false)){
-		cout << img_salida << " guardado correctamente \n";
-		cout << "usa: display " << img_salida << " para ver el resultado\n";
+	for(int k = 0; k <= lista->longitud(); k++){
+		Imagen * aux = lista->get(k);
+		char nombre[MAX_NOMBRE] = "file_";
+		sprintf(nombre,"%d",k );
+		if (aux->escribirImagen(nombre, false)){
+			cout << nombre << " guardado correctamente \n";
+			cout << "usa: display " << nombre << " para ver el resultado\n";
+		}
+		else{
+			cerr << "Error guardando la imagen " << nombre << endl;
+			delete imagen1;
+			delete imagen2;
+			return 1;
+		}
 	}
-	else{
-    	cerr << "Error guardando la imagen " << img_salida << endl;
-		return 1;
-	}
+
+	delete imagen1;
+	delete imagen2;
+	delete lista;
+
+	return 0;
 }
